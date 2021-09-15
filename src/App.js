c451ca14d7f8e5d2b8a47d5dcaa03d8af138d081
anianeal <74513210+anianeal@users.noreply.github.com>
2021-09-15 18:17:27 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import AddIssue from "./components/AddIssue/AddIssue";
+import CurrentIssue from "./components/CurrentIssue/CurrentIssue";
+
+import "./App.css";
+
+function App() {
+  const [addIssue, setAddIssue] = useState(true);
+
+  function showAddIssue() {
+    setAddIssue(true);
+  }
+
+  function showCurrentIssue() {
+    setAddIssue(false);
+  }
+
+  return (
+    <div className="App">
+      <h1>Issue Tracker</h1>
+      <div className="grey-card-contianer">
+        {addIssue ? <AddIssue /> : <CurrentIssue />}
+      </div>
+      <div className="view-selection-div">
+        <button class="view-button" onClick={() => showCurrentIssue()}>
+          Current Issues
+        </button>
+        <button
+          class="view-button active-button"
+          onClick={() => showAddIssue()}
+        >
+          Add Issue
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
